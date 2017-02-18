5ca1e8788eb2725d4d7e69345f8ef55ee9218748
Michael Lee <michael_lee.org@hotmail.com>
2017-02-17 20:21:10 -0500

    Initial commit

---
+++ Jalapeno/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jalapeno
+//
+//  Created by Michael Lee on 2/17/17.
+//  Copyright © 2017 Jalapeno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jalapeno/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Jalapeno
+//
+//  Created by Michael Lee on 2/17/17.
+//  Copyright © 2017 Jalapeno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
